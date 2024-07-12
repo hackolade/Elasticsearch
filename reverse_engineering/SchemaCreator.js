7bef211363fab00db076be81235a18c36805a2a4
chulanovskyi-bs <56116665+chulanovskyi-bs@users.noreply.github.com>
2024-07-12 18:35:57 +0300

    fix: early brace (#15)
    * fix: early brace
    
    * fix: sonar remarks
    
    * fix: attempt to fix sonar remark of potential performance issue of the regexp

--- reverse_engineering/SchemaCreator.js
+++ reverse_engineering/SchemaCreator.js
@@ -102,7 +102,7 @@ module.exports = {
 		let schema = {};
 
 		for (let fieldName in properties) {
-			const currentSample = sample && sample[fieldName];
+			const currentSample = sample?.[fieldName];
 
 			schema[fieldName] = this.getField(properties[fieldName], currentSample, mapping);
 		}
@@ -251,8 +251,7 @@ module.exports = {
 
 				return {};
 		}
-	}
-},
+	},
 
 	getScalar(value) {
 		return typeof value;
@@ -266,17 +265,21 @@ module.exports = {
 
 		if (isFloat) {
 			return 'float';
-		} else {
-			if (value > -(byte + 1) && value < byte) {
-				return 'byte';
-			} else if (value > -(short + 1) && value < short) {
-				return 'short';
-			} else if (value > -(int + 1) && value < int) {
-				return 'integer';
-			} else {
-				return 'long';
-			}
 		}
+
+		if (value > -(byte + 1) && value < byte) {
+			return 'byte';
+		}
+
+		if (value > -(short + 1) && value < short) {
+			return 'short';
+		}
+
+		if (value > -(int + 1) && value < int) {
+			return 'integer';
+		}
+
+		return 'long';
 	},
 
 	getServiceFields(sample) {
@@ -302,7 +305,7 @@ module.exports = {
 
 	getGeoPointSubtype(value) {
 		if (typeof value === 'string') {
-			if (/\-?\d+\.\d+\,\-?\d+\.\d+/.test(value)) {
+			if (/^-?\d+\.\d+,-?\d+\.\d+$/.test(value)) {
 				return 'string';
 			} else {
 				return 'geohash';
@@ -426,9 +429,13 @@ module.exports = {
 	},
 
 	setProperties(schema, fieldData) {
-		const properties = helper.getFieldProperties(schema.type, { mode: fieldData.type, ...fieldData}, {
-			'stringfields': 'fields',
-		});
+		const properties = helper.getFieldProperties(
+			schema.type,
+			{ mode: fieldData.type, ...fieldData },
+			{
+				'stringfields': 'fields',
+			},
+		);
 
 		for (let propName in properties) {
 			if (propName === 'fields') {
